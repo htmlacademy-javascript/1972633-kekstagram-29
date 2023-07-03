bf988825b357ea223de05f5bc69a8406e311acb9
ElenaEfimova-1985 <elenaefimova153@gmail.com>
2023-07-03 16:24:39 +0300

    Открывается и закрывается

--- js/picture.js
+++ js/picture.js
@@ -1,20 +1,27 @@
 import { someCreateDescription } from './data.js';
 
-const picturesArrey = someCreateDescription();
+const picturesArray = someCreateDescription();
 const pictureTemplate = document.querySelector('#picture').content;
 const picturesListFragment = document.createDocumentFragment();
 
-picturesArrey.forEach((picture) => {
+const createItem = (item) =>{
   const pictureItemTemplate = pictureTemplate.querySelector('.picture');
   const pictureItem = pictureItemTemplate.cloneNode(true);
   const pictureImg = pictureItem.querySelector('.picture__img');
   const pictureComments = pictureItem.querySelector('.picture__comments');
   const pictureLikes = pictureItem.querySelector('.picture__likes');
 
-  pictureImg.src = picture.url;
-  pictureImg.alt = picture.description;
-  pictureComments.textContent = picture.comments.length;
-  pictureLikes.textContent = picture.likes;
-  picturesListFragment.append(pictureItem);
+  pictureImg.src = item.url;
+  pictureImg.alt = item.description;
+  pictureComments.textContent = item.comments.length;
+  pictureLikes.textContent = item.likes;
+  pictureItem.dataset.id = item.id;
+  return pictureItem;
+};
+
+picturesArray.forEach((picture) => {
+  picturesListFragment.append(createItem(picture));
 });
+
 export { picturesListFragment };
+export { picturesArray };
